11738276482e5c6de8ae822d689adf8d9d67f363
Binh SE <binhtk1@gmail.com>
2020-12-09 13:29:55 +0700

    Seeding Database

---
+++ mktSolution.Data/Extensions/ModelBuilderExtension.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mktSolution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mktSolution.Data.Extensions
+{
+    public static class ModelBuilderExtension
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SCM_Order>().HasData(
+                new SCM_Order() { IdOrder = 1, ModelName = "A", Qty = 1000, DueDate = new DateTime(2020, 12, 1, 10, 30, 50) },
+                new SCM_Order() { IdOrder = 2, ModelName = "B", Qty = 2000, DueDate = new DateTime(2020, 12, 3, 10, 30, 50) },
+                new SCM_Order() { IdOrder = 3, ModelName = "A", Qty = 1300, DueDate = new DateTime(2020, 12, 5, 10, 30, 50) },
+                new SCM_Order() { IdOrder = 4, ModelName = "C", Qty = 5000, DueDate = new DateTime(2020, 12, 7, 10, 30, 50) }
+                );
+
+            modelBuilder.Entity<SCM_Product>().HasData(
+                new SCM_Product() { IdProduct = 1, ModelName = "A", Qty = 500, DateProduct = new DateTime(2020, 12, 2, 10, 30, 50), IdOrder = 1, Jobname = "JN0001" },
+                new SCM_Product() { IdProduct = 2, ModelName = "B", Qty = 800, DateProduct = new DateTime(2020, 12, 3, 10, 30, 50), IdOrder = 2, Jobname = "JN0002" },
+                new SCM_Product() { IdProduct = 3, ModelName = "A", Qty = 600, DateProduct = new DateTime(2020, 12, 3, 10, 30, 50), IdOrder = 1, Jobname = "JN0003" },
+                new SCM_Product() { IdProduct = 4, ModelName = "B", Qty = 1000, DateProduct = new DateTime(2020, 12, 4, 10, 30, 50), IdOrder = 2, Jobname = "JN0004" }
+                );
+
+            modelBuilder.Entity<SCM_Calendar>().HasData(
+                new SCM_Calendar() { IdCalendar = 1, Date = new DateTime(2020, 12, 1, 10, 30, 50) },
+                new SCM_Calendar() { IdCalendar = 2, Date = new DateTime(2020, 12, 2, 10, 30, 50) },
+                new SCM_Calendar() { IdCalendar = 3, Date = new DateTime(2020, 12, 3, 10, 30, 50) },
+                new SCM_Calendar() { IdCalendar = 4, Date = new DateTime(2020, 12, 4, 10, 30, 50) },
+                new SCM_Calendar() { IdCalendar = 5, Date = new DateTime(2020, 12, 5, 10, 30, 50) },
+                new SCM_Calendar() { IdCalendar = 6, Date = new DateTime(2020, 12, 6, 10, 30, 50) },
+                new SCM_Calendar() { IdCalendar = 7, Date = new DateTime(2020, 12, 7, 10, 30, 50) }
+                );
+        }
+    }
+}
---
+++ mktSolution.Data/Migrations/20201209062429_SeedDB.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mktSolution.Data.Migrations
+{
+    public partial class SeedDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "SCM_Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 12, 9, 13, 24, 28, 455, DateTimeKind.Local).AddTicks(9110),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 12, 9, 12, 10, 58, 63, DateTimeKind.Local).AddTicks(8351));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "SCM_Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 12, 9, 13, 24, 28, 440, DateTimeKind.Local).AddTicks(1711),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 12, 9, 12, 10, 58, 41, DateTimeKind.Local).AddTicks(8095));
+
+            migrationBuilder.InsertData(
+                table: "SCM_Calendar",
+                columns: new[] { "IdCalendar", "Date", "Descreiption", "WeekNo" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2020, 12, 1, 10, 30, 50, 0, DateTimeKind.Unspecified), null, 0 },
+                    { 2, new DateTime(2020, 12, 2, 10, 30, 50, 0, DateTimeKind.Unspecified), null, 0 },
+                    { 3, new DateTime(2020, 12, 3, 10, 30, 50, 0, DateTimeKind.Unspecified), null, 0 },
+                    { 4, new DateTime(2020, 12, 4, 10, 30, 50, 0, DateTimeKind.Unspecified), null, 0 },
+                    { 5, new DateTime(2020, 12, 5, 10, 30, 50, 0, DateTimeKind.Unspecified), null, 0 },
+                    { 6, new DateTime(2020, 12, 6, 10, 30, 50, 0, DateTimeKind.Unspecified), null, 0 },
+                    { 7, new DateTime(2020, 12, 7, 10, 30, 50, 0, DateTimeKind.Unspecified), null, 0 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "SCM_Orders",
+                columns: new[] { "IdOrder", "CreateBy", "DueDate", "ModelName", "Qty", "UpdateBy", "UpdateDate" },
+                values: new object[,]
+                {
+                    { 1, 0, new DateTime(2020, 12, 1, 10, 30, 50, 0, DateTimeKind.Unspecified), "A", 1000, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, 0, new DateTime(2020, 12, 3, 10, 30, 50, 0, DateTimeKind.Unspecified), "B", 2000, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, 0, new DateTime(2020, 12, 5, 10, 30, 50, 0, DateTimeKind.Unspecified), "A", 1300, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, 0, new DateTime(2020, 12, 7, 10, 30, 50, 0, DateTimeKind.Unspecified), "C", 5000, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "SCM_Products",
+                columns: new[] { "IdOrder", "IdProduct", "CreateBy", "DateProduct", "Jobname", "ModelName", "Qty", "UpdateBy", "UpdateDate" },
+                values: new object[,]
+                {
+                    { 1, 1, 0, new DateTime(2020, 12, 2, 10, 30, 50, 0, DateTimeKind.Unspecified), "JN0001", "A", 500, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 1, 3, 0, new DateTime(2020, 12, 3, 10, 30, 50, 0, DateTimeKind.Unspecified), "JN0003", "A", 600, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, 2, 0, new DateTime(2020, 12, 3, 10, 30, 50, 0, DateTimeKind.Unspecified), "JN0002", "B", 800, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, 4, 0, new DateTime(2020, 12, 4, 10, 30, 50, 0, DateTimeKind.Unspecified), "JN0004", "B", 1000, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SCM_Calendar",
+                keyColumn: "IdCalendar",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Calendar",
+                keyColumn: "IdCalendar",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Calendar",
+                keyColumn: "IdCalendar",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Calendar",
+                keyColumn: "IdCalendar",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Calendar",
+                keyColumn: "IdCalendar",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Calendar",
+                keyColumn: "IdCalendar",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Calendar",
+                keyColumn: "IdCalendar",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Orders",
+                keyColumn: "IdOrder",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Orders",
+                keyColumn: "IdOrder",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Products",
+                keyColumns: new[] { "IdOrder", "IdProduct" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Products",
+                keyColumns: new[] { "IdOrder", "IdProduct" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Products",
+                keyColumns: new[] { "IdOrder", "IdProduct" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Products",
+                keyColumns: new[] { "IdOrder", "IdProduct" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Orders",
+                keyColumn: "IdOrder",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "SCM_Orders",
+                keyColumn: "IdOrder",
+                keyValue: 2);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "SCM_Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 12, 9, 12, 10, 58, 63, DateTimeKind.Local).AddTicks(8351),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 12, 9, 13, 24, 28, 455, DateTimeKind.Local).AddTicks(9110));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "SCM_Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 12, 9, 12, 10, 58, 41, DateTimeKind.Local).AddTicks(8095),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 12, 9, 13, 24, 28, 440, DateTimeKind.Local).AddTicks(1711));
+        }
+    }
+}
